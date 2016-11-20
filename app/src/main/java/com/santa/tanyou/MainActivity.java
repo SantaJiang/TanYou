42c4a206fc02b9bbf7ce3505787dde72d9891f9e
Santa <lilin.santa@gmail.com>
2016-11-21 07:44:04 +0800

    实现高德地图追踪实时定位功能

--- app/src/main/java/com/santa/tanyou/MainActivity.java
+++ app/src/main/java/com/santa/tanyou/MainActivity.java
@@ -2,16 +2,30 @@ package com.santa.tanyou;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 
+import com.amap.api.location.AMapLocation;//定位信息类
+import com.amap.api.location.AMapLocationClient;//定位服务类
+import com.amap.api.location.AMapLocationClientOption;//定位参数设置（在定位服务时需要这些参数）
+import com.amap.api.location.AMapLocationListener;//定位回调接口
 import com.amap.api.maps.AMap;
+import com.amap.api.maps.LocationSource;
 import com.amap.api.maps.MapView;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements LocationSource, AMapLocationListener {
     private AMap aMap;
     private MapView mapView;
+    private OnLocationChangedListener mListener;
+    private AMapLocationClient mlocationClient;
+    private AMapLocationClientOption mLocationOption;
+
+    private TextView mLocationErrText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +37,14 @@ public class MainActivity extends Activity {
         mapView.onCreate(savedInstanceState);
         init();
 
-        ToggleButton tb =(ToggleButton) findViewById(R.id.tb);
+        ToggleButton tb = (ToggleButton) findViewById(R.id.tb);
         tb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked){
+                if (isChecked) {
                     //设置使用卫星地图
                     aMap.setMapType(AMap.MAP_TYPE_SATELLITE);
-                }else {
+                } else {
                     //设置使用普通地图
                     aMap.setMapType(AMap.MAP_TYPE_NORMAL);
                 }
@@ -39,17 +53,34 @@ public class MainActivity extends Activity {
     }
 
     //初始化AMap对象
-    private void init(){
-        if (aMap == null){
+    private void init() {
+        if (aMap == null) {
             aMap = mapView.getMap();
+            setUpMap();
         }
+        mLocationErrText = (TextView) findViewById(R.id.location_errInfo_text);
+        mLocationErrText.setVisibility(View.GONE);
     }
+
+    /**
+     * 设置一些amap的属性
+     */
+    private void setUpMap() {
+        aMap.setLocationSource(this);// 设置定位监听
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);// 设置默认定位按钮是否显示
+        aMap.setMyLocationEnabled(true);// 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
+        // 设置定位的类型为定位模式 ，可以由定位、跟随或地图根据面向方向旋转几种
+        aMap.setMyLocationType(AMap.LOCATION_TYPE_MAP_FOLLOW);//定位模式
+    }
+
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
         mapView.onDestroy();
+        if (null != mlocationClient) {
+            mlocationClient.onDestroy();
+        }
     }
 
     @Override
@@ -65,6 +96,61 @@ public class MainActivity extends Activity {
         //在activity执行onPause时执行mMapView.onPause ()，实现地图生命周期管理
         mapView.onPause();
     }
+
+    /**
+     * 定位成功后回调函数
+     */
+    @Override
+    public void onLocationChanged(AMapLocation amapLocation) {
+        if (mListener != null && amapLocation != null) {
+            if (amapLocation != null && amapLocation.getErrorCode() == 0) {
+                mLocationErrText.setVisibility(View.GONE);
+                mListener.onLocationChanged(amapLocation);// 显示系统小蓝点
+            } else {
+                String errText = "定位失败," + amapLocation.getErrorCode() + ": " + amapLocation.getErrorInfo();
+                Log.e("AmapErr", errText);
+                mLocationErrText.setVisibility(View.VISIBLE);
+                mLocationErrText.setText(errText);
+            }
+        }
+    }
+
+    /**
+     * 激活定位
+     */
+    @Override
+    public void activate(OnLocationChangedListener listener) {
+        mListener = listener;
+        if (mlocationClient == null) {
+            mlocationClient = new AMapLocationClient(this);
+            mLocationOption = new AMapLocationClientOption();
+            //设置定位监听
+            mlocationClient.setLocationListener(this);
+            //设置为高精度定位模式
+            mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+            //设置定位参数
+            mlocationClient.setLocationOption(mLocationOption);
+            // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+            // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+            // 在定位结束后，在合适的生命周期调用onDestroy()方法
+            // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+            mlocationClient.startLocation();
+        }
+    }
+
+    /**
+     * 停止定位
+     */
+    @Override
+    public void deactivate() {
+        mListener = null;
+        if (mlocationClient != null) {
+            mlocationClient.stopLocation();
+            mlocationClient.onDestroy();
+        }
+        mlocationClient = null;
+    }
+
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
